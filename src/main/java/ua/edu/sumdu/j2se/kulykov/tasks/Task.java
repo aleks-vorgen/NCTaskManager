e58d06de471b4c5b6d72f560d8c71440a6562548
ALexus <vorgen.morgen@yandex.ru>
2022-05-09 16:59:15 +0200

    Created class Task + passed 1-2 tests

---
+++ src/main/java/ua/edu/sumdu/j2se/kulykov/tasks/Task.java
@@ -0,0 +1,138 @@
+package ua.edu.sumdu.j2se.kulykov.tasks;
+
+/**
+ * Class declared some Tasks.
+ * @author Kulykov
+ * @version 1.0
+ */
+public class Task {
+
+    private String title;
+    private int time;
+    private int time_start;
+    private int time_end;
+    private int time_interval_repeat;
+    private boolean isActive;
+    private boolean isRepeated;
+
+
+    /**
+     * Constructor describing the Task with title and time of action.
+     * @param title title of the task
+     * @param time execution time
+     */
+    public Task(String title, int time) {
+        this.title = title;
+        this.time = time;
+        isActive = false;
+        isRepeated = false;
+    }
+
+    /**
+     * Constructor describing the Task with title,
+     * interval of action and repetition.
+     * @param title title of the task
+     * @param time_start time when action starts
+     * @param time_end time when action ends
+     * @param time_interval_repeat task repetition interval
+     */
+    public Task(String title, int time_start, int time_end, int time_interval_repeat) {
+        this.title = title;
+        this.time_start = time_start;
+        this.time_end = time_end;
+        this.time_interval_repeat = time_interval_repeat;
+        isActive = false;
+        isRepeated = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Method returns repetition time if the task is repeated
+     * or returns execution time if is.
+     */
+    public int getTime() {
+        if (isRepeated) return time_start;
+        else return time;
+    }
+
+    /**
+     * Method sets the time for the task and make it not repetitive.
+     * @param time execution time
+     */
+    public void setTime(int time) {
+        this.time = time;
+        if (isRepeated) {
+            isRepeated = false;
+        }
+    }
+
+    /**
+     * Method sets the time when action starts and time when action ends
+     * for the not repetitive task.
+     */
+    public void setTime(int time_start, int time_end, int time_interval_repeat) {
+        this.time_start = time_start;
+        this.time_end = time_end;
+        this.time_interval_repeat = time_interval_repeat;
+        if (!isRepeated) isRepeated = true;
+    }
+
+    /**
+     * Method returns execution time if the task is not repetitive
+     * or returns time when action starts if the task is repetitive.
+     */
+    public int getStartTime() {
+        return isRepeated ? time_start : time;
+    }
+    //public void setStartTime(int time_start) { this.time_start = time_start; }
+
+    /**
+     * Method returns execution time if the task is not repetitive
+     * or returns time when action ends if the task is repetitive.
+     */
+    public int getEndTime() {
+        return isRepeated ? time_end : time;
+    }
+    //public void setTime_end(int time_end) { this.time_end = time_end; }
+
+    /**
+     * Method returns repetition interval if the task is repetitive
+     * or returns 0 if the task is not repetitive.
+     */
+    public int getRepeatInterval() {
+        return isRepeated ? time_interval_repeat : 0;
+    }
+    public void setTime_interval_repeat(int time_interval_repeat) {
+        this.time_interval_repeat = time_interval_repeat;
+    }
+
+    public boolean isActive() { return isActive; }
+    public void setActive(boolean isActive) { this.isActive = isActive; }
+
+    public boolean isRepeated() { return isRepeated; }
+    public void setRepeated(boolean isRepeated) { this.isRepeated = isRepeated; }
+
+    public int nextTimeAfter(int current) {
+        if(!isActive) return -1;
+
+        int next_time;
+        if (isRepeated) {
+            next_time = time_start;
+        }
+        else {
+            next_time = time;
+            return next_time > current ? next_time : -1;
+        }
+
+        while (next_time <= current)
+            next_time += time_interval_repeat;
+
+        return next_time < time_end ? next_time : -1;
+    }
+}
