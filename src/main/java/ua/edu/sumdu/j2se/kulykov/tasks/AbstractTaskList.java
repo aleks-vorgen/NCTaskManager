2de2c357754663a0f2b72511d3e24ec0e8a35277
ALexus <vorgen.morgen@yandex.ru>
2022-06-09 00:07:41 +0200

    Created iterators for each list and service methods for each class.

--- src/main/java/ua/edu/sumdu/j2se/kulykov/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/kulykov/tasks/AbstractTaskList.java
@@ -1,14 +1,16 @@
 package ua.edu.sumdu.j2se.kulykov.tasks;
 
+
 public abstract class AbstractTaskList implements Iterable<Task> {
 
     protected abstract Task getTask(int index);
     protected abstract int size();
     protected abstract void add(Task task);
     protected abstract boolean remove(Task task);
+    protected abstract ListTypes.types getType();
 
     public AbstractTaskList incoming(int from, int to) {
-        AbstractTaskList res = TaskListFactory.createTaskList(ListTypes.types.LINKED);
+        AbstractTaskList res = TaskListFactory.createTaskList(getType());
         for (int i = 0; i < size(); i++) {
             if (getTask(i).isActive() && getTask(i).nextTimeAfter(from) < to && getTask(i).nextTimeAfter(from) > from) {
                 res.add(getTask(i));
